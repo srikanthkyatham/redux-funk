fb33161fe2acc3ae3a7997ed33cef1fc736aab08
sriky <srikanth.kyatham@kaltiot.com>
2017-06-14 09:30:51 +0300

    Adding coalesceFunksImmutable, runFunksImmutable

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 'use strict'
 const queue = Symbol('actions')
+const { Map } = require('immutable')
 
 module.exports.call = (action, funk) => {
   action[queue] = (action[queue] || []).concat([funk])
@@ -16,6 +17,19 @@ module.exports.coalesceFunks = reducer => (state, action) => {
   return Object.assign({}, nextState, {funks})
 }
 
+// function from reducer to another reducer
+// the new reducer adds a 'funks' key with declarative effects
+// of type [func, [args]]
+// returns an Immutable object
+module.exports.coalesceFunksImmutable = reducer => (state, action) => {
+  const nextState = reducer(state, action)
+  const funks = action[queue] || []
+  // restore action to the way it was
+  delete action[queue]
+  const map = Map(nextState)
+  return map.set('funks', funks)
+}
+
 // listen for store updates, and run each funk.
 // `runFunks` assumes that each funk either returns nothing
 // or returns a promise for an action.
@@ -25,6 +39,27 @@ module.exports.coalesceFunks = reducer => (state, action) => {
 module.exports.runFunks = store => {
   store.subscribe(() => {
     const funks = store.getState().funks || []
+    funks.forEach(function(funk) {
+      const func = funk[0]
+      const args = funk[1]
+      const maybePromiseForAction = func.apply(null, args)
+      if (!maybePromiseForAction || !maybePromiseForAction.then) {
+        return
+      }
+      maybePromiseForAction.then(store.dispatch)
+    })
+  })
+}
+
+// listen for store updates, and run each funk.
+// `runFunksImmutable` assumes that each funk either returns nothing
+// or returns a promise for an action.
+// `runFunksImmutable` dispatches the actions
+// You can replace `runFunksImmutable` with your own implementation
+// if you prefer callbacks over promises, for example
+module.exports.runFunksImmutable = store => {
+  store.subscribe(() => {
+    const funks = store.getState().get('funks') || []
     funks.forEach(function(funk) {
       const func = funk[0]
       const args = funk[1]
--- test.js
+++ test.js
@@ -1,8 +1,9 @@
 'use strict'
 const { deepEqual } = require('assert')
 const { createStore } = require('redux')
-const { coalesceFunks, funkMiddleware, call, runFunks } = require('./src/index.js')
+const { coalesceFunks, coalesceFunksImmutable, funkMiddleware, call, runFunks, runFunksImmutable } = require('./src/index.js')
 const test = require('tape')
+const { Map } = require('immutable')
 
 const asyncAction = payload => new Promise((resolve, reject) => {
   resolve({type: 'SECOND', payload})
@@ -20,8 +21,25 @@ const reducer = coalesceFunks((state = {text: 'initial'}, action) => {
   }
 })
 
+const reducerImmutable = coalesceFunksImmutable((state = Map({text: 'initial'}), action) => {
+  switch (action.type) {
+    case 'FIRST': {
+      call(action, [asyncAction, ['payload']])
+      const map = Map(state)
+      return map.set("text", 'foo')
+    }
+    case 'SECOND': {
+      const map = Map(state)
+      return map.set("text", action.payload)
+    }
+    default:
+      return state
+  }
+})
+
+
 test('integration: `call` and `coalesceFunks`', t => {
-  const { dispatch, getState } = createStore(reducer)  
+  const { dispatch, getState } = createStore(reducer)
   const action = {type: 'FIRST'}
   dispatch(action)
   t.deepEqual(
@@ -45,6 +63,32 @@ test('integration: `call` and `coalesceFunks`', t => {
   t.end()
 })
 
+test('integration: `call` and `coalesceFunks Immutable`', t => {
+
+  const { dispatch, getState } = createStore(reducerImmutable, Map())
+  const action = {type: 'FIRST'}
+  dispatch(action)
+  t.deepEqual(
+            getState().get('funks')[0],
+            [asyncAction, ['payload']],
+            'funks are added to `state`')
+  t.deepEqual(
+            Object.getOwnPropertySymbols(action),
+            [],
+            'reducer is pure (action restored to the way it was)')
+  t.deepEqual(
+            getState().getIn(["text"]),
+            'foo',
+            'rest of state is updated correctly')
+  dispatch({type: 'SECOND', payload: 'new payload'})
+  t.deepEqual(
+            getState().toJS(),
+            {funks: [], text: 'new payload'},
+            'funks are reset on each action'
+            )
+  t.end()
+})
+
 test('`runFunks`', t => {
   // BEGIN FIXTURES
   let calledWith
@@ -58,7 +102,7 @@ test('`runFunks`', t => {
         call(action, [mock, [action.payload]])
         return state
       case 'BAR':
-        return Object.assign(state, {'funksHaveRun': true}) 
+        return Object.assign(state, {'funksHaveRun': true})
     }
   })
   const store = createStore(reducer)
@@ -81,6 +125,47 @@ test('`runFunks`', t => {
       true,
       'dispatches actions'
     )
-    t.end()  
+    t.end()
+  })
+})
+
+test('`runFunks Immutable`', t => {
+  // BEGIN FIXTURES
+  let calledWith
+  const mock = arg => {
+    calledWith = arg
+    return Promise.resolve({type: 'BAR'})
+  }
+  const reducer = coalesceFunksImmutable((state=Map(), action) => {
+    switch (action.type) {
+      case 'FOO':
+        call(action, [mock, [action.payload]])
+        return state
+      case 'BAR':
+        const map = Map(state)
+        return map.set('funksHaveRun', true)
+    }
+  })
+  const store = createStore(reducer)
+  const { dispatch, getState } = store
+  runFunksImmutable(store)
+  const action = {type: 'FOO', payload: 'hi'}
+  dispatch(action)
+  // END FIXTURES
+
+  t.deepEqual(
+    calledWith,
+    action.payload,
+    'calls funks'
+  )
+
+  // wait to next tick of event loop for promise to resolve
+  setTimeout(() => {
+    t.deepEqual(
+      getState().get('funksHaveRun'),
+      true,
+      'dispatches actions'
+    )
+    t.end()
   })
 })
